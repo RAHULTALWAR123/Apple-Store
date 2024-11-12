05f5335ea5ec7e6036858db19123ad1723709956
Rahul <talwarrahul295@gmail.com>
2024-11-12 14:03:20 +0530

    correction2

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -46,6 +46,10 @@ export const getAllOrders = async (req, res) => {
 	try {
 		const orders = await Order.find({});
 
+		if (orders.length === 0) {
+			return res.status(404).json({ message: "No orders found" });
+		}
+
 		res.status(200).json(orders);
 	} catch (error) {
 		res.status(500).json({ message: "Unable to fetch orders", error });
